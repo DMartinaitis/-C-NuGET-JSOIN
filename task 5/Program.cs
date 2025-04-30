5a086e33a1dd1c4e7b5a8f3e43645cf861d02863
DMartinaitis <martinaitisdovydas@gmail.com>
2025-04-30 21:44:41 +0300

    Add files via upload

---
+++ task 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+class Program
+{
+    public class UserWithRole
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Role { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        string json = File.ReadAllText("user_types.json");
+        var users = JsonConvert.DeserializeObject<List<UserWithRole>>(json);
+
+        Console.WriteLine("User Types:");
+        foreach (var user in users)
+        {
+            Console.WriteLine($"Name: {user.Name}, Age: {user.Age}, City: {user.City}, Role: {user.Role}");
+        }
+    }
+}
+
+
